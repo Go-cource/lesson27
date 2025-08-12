244568ce74ca4ddec4630a392317bef0b0ed38a2
Fisherman August <fshrmn.august@gmail.com>
2025-08-12 21:11:18 +0300

    jwt

---
+++ go.mod
@@ -0,0 +1,5 @@
+module lesson27
+
+go 1.23.6
+
+require github.com/golang-jwt/jwt/v4 v4.5.2 // indirect
---
+++ jwt.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/golang-jwt/jwt/v4"
+)
+
+type Claims struct {
+	jwt.RegisteredClaims
+	UserId   int
+	UserName string
+}
+
+const TOKEN_EXPIRES_AT = time.Hour * 2
+const SECRET_KEY = "supersecretkey"
+
+func BuildJWTString() (string, error) {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, Claims{
+		RegisteredClaims: jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(TOKEN_EXPIRES_AT)),
+		},
+		UserId:   1,
+		UserName: "Oleg",
+	})
+	tokenString, err := token.SignedString([]byte(SECRET_KEY))
+	if err != nil {
+		return "", err
+	}
+	return tokenString, nil
+}
+
+func main() {
+	tokenString, err := BuildJWTString()
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(tokenString)
+}
